cd22d3a100f9708b83570dad8c482fcdffc73e7a
wtke <kewantingx@gmail.com>
2021-02-21 23:45:04 +0800

    expansion panel

--- src/app/corr-cp/corr-cp.component.ts
+++ src/app/corr-cp/corr-cp.component.ts
@@ -6,12 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./corr-cp.component.css']
 })
 export class CorrCpComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
   typesOfShoes;
+  days;
 
   ngOnInit(): void {
     this.typesOfShoes = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+    this.days = [
+      {
+        date: "Mon 01", 
+        subDay: [
+          {
+            name: "morning"
+          }, 
+          {
+            name:"afternoon"
+          },
+          {
+            name: "evening"
+          },
+          {
+            name: "night"
+          }
+        ]
+      },
+      {
+        date: "Tue 02", 
+        subDay: [
+          {
+            name: "morning"
+          }, 
+          {
+            name:"afternoon"
+          },
+          {
+            name: "evening"
+          },
+          {
+            name: "night"
+          }
+        ]
+      },
+      {
+        date: "Wed 03", 
+        subDay: [
+          {
+            name: "morning"
+          }, 
+          {
+            name:"afternoon"
+          },
+          {
+            name: "evening"
+          },
+          {
+            name: "night"
+          }
+        ]
+      }
+    ];
   }
 
+  
 }
